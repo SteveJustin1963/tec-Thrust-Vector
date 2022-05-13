9bc75e30995a788bf29f717bf53dd9cddf5995ef
Stephen Justin <stevejustin1963@gmail.com>
2022-05-13 23:52:37 +1000

    Update tvbal_t.c

--- tvbal_t.c
+++ tvbal_t.c
@@ -38,6 +38,27 @@ void trajectory_control(double *position, double *velocity, double *acceleration
     position[2] += velocity[2] * dt;
 }
 
+\\\\\\\\\\\\\\\\\\\
+matlab
+
+
+    function [position, velocity, acceleration] = trajectory_control(position, velocity, acceleration, thrust, mass, drag, desired_position, desired_velocity, desired_acceleration)
+    % Calculate drag force
+    F_drag = -0.5 * drag * velocity * velocity;
+    % Calculate thrust force
+    F_thrust = thrust * mass;
+    % Calculate desired force
+    F_desired = (desired_position - position) * kp + (desired_velocity - velocity) * kv + (desired_acceleration - acceleration) * ka;
+    % Calculate net force
+    acceleration = (F_thrust + F_drag + F_desired) / mass;
+    % Update velocity
+    velocity = velocity + acceleration * dt;
+    % Update position
+    position = position + velocity * dt;
+end
+
+
+
 ; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 convert the above to fortran IV
